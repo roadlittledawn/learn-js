f1be0fb2e86c91f93eae491dc3e5a81fa7c7db53
Clinton <clinton.langosch@gmail.com>
2020-06-24 14:49:12 -0700

    copy paste plg eng exercise. refine later

--- src/linkedList.js
+++ src/linkedList.js
@@ -68,5 +68,89 @@ console.log(list.getFirst())
 
 
 
+// From PLG interview exercise
 
+class Node { 
+  constructor(data, next = null) { 
+    this.data = data; 
+    this.next = next;
+  } 
+}
+
+class LinkedList { 
+  constructor(head = null) { 
+    this.head = head; 
+  }
+  
+  
+  // Last in (insert)
+  
+  // Add 2
+  insert(data) {
+    let newNode = new Node(data); //node (2)
+    newNode.next = this.head; //node.next = node(1)
+    this.head = newNode; //head = node (2)
+  }
+  
+  // First out
+  // function here 
+  remove() {
+    // Head node(2)
+    // Remove node(2) we would want head to be node(1)
+    this.head = this.head.next;
+  }
+  
+  removeMiddle() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count++;
+      node = node.next
+    }
+    node = this.head;
+    for (let i = 1; i < Math.ceil(count/2); i++) {
+      node = node.next;
+    }
+    console.log(node.data); // node 4
+    console.log(node.next.next.data); // node 2
+    if (node.next.next === null) {
+      
+    }
+    if (node.next.next) {
+      
+    }
+    
+    // 1 2 3 4 5
+    //     ^
+    //         ^
+ 
+    
+  }
+  
+  printList() {
+    let currNode = this.head;
+    console.log('LinkedList: ');
+    
+    while (currNode != null) {
+      console.log(currNode.data);
+      currNode = currNode.next;
+    }
+  }
+} 
+
+
+let list = new LinkedList();
+         
+list.insert(1);
+list.insert(2); // This node this.next would be what? 
+list.insert(3);
+list.insert(4);
+list.insert(5);
+
+//list.remove();
+// 3 2 1
+
+list.removeMiddle();
+         
+list.printList();
 
